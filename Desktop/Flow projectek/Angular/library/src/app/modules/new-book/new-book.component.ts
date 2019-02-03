ed8c0170c70be36348b423115e7c2866bfe36fb7
Netti992 <n.anetta@gmail.hu>
2019-02-03 16:36:00 +0100

    made new book page

--- Desktop/Flow projectek/Angular/library/src/app/modules/new-book/new-book.component.ts
+++ Desktop/Flow projectek/Angular/library/src/app/modules/new-book/new-book.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-new-book',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-book.component.css']
 })
 export class NewBookComponent implements OnInit {
+  
+  public newBookForm: FormGroup; 
 
   constructor() { }
 
   ngOnInit() {
+    this.newBookForm = new FormGroup({
+      title: new FormControl(),
+      lastName: new FormControl(),
+      firstName: new FormControl(),
+      summary: new FormControl(),
+      category: new FormControl(),
+      publishedDate: new FormControl(),
+      publisher: new FormControl(),
+      pages: new FormControl(),
+      isbn: new FormControl()
+    })
   }
 
 }
